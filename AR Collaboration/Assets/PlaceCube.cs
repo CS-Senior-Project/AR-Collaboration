1b5f97a361a735e4353f369420685fa0d6edafe2
Carson Pemble <43591853+PembleC@users.noreply.github.com>
2020-01-27 10:57:36 -0800

    Steam VR and Hands
    Steam VR allows for controller input and also for controller tracking. I followed the tutorial "Motion Controllers in AR" fro stereolabs for the hands

--- AR Collaboration/Assets/PlaceCube.cs
+++ AR Collaboration/Assets/PlaceCube.cs
@@ -15,13 +15,20 @@ public class PlaceCube : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            Vector2 screenpos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            Vector3 worldpos;
+
             GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
             cube.transform.position = new Vector3(1, 1, 1);
 
             Rigidbody cubeBody = cube.GetComponent<Rigidbody>();
             cubeBody.useGravity = true;
 
-
-}
+            /*            if (ZEDSupportFunctions.GetWorldPositionAtPixel(screenpos, ZEDManager.Instance.GetLeftCameraTransform().GetComponent<Camera>(), out worldpos))
+                        {
+                            GetComponent<Rigidbody>().velocity = Vector3.zero;
+                            transform.position = worldpos + Vector3.up;
+                        }*/
+        }
     }
 }
---
+++ AR Collaboration/Assets/ViveInput.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;     //Steam VR Actions
+
+public class ViveInput : MonoBehaviour { 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
