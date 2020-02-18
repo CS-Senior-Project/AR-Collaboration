ca9b7b0ab3dde9ecb45105984d7449a10d90b5f1
Carson Pemble <43591853+PembleC@users.noreply.github.com>
2020-02-17 22:32:57 -0800

    Login Screen
    Register and NewLogin are the two key files
    Login Scene is the Very first scene to our app

---
+++ AR Collaboration/Assets/Login.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Login : MonoBehaviour
+{
+
+    private string u = "username";
+    private string p = "password";
+
+    private string usernameString = string.Empty;
+    private string passwordString = string.Empty;
+
+    private Rect windowRect = new Rect(0, 0, Screen.width, Screen.height);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnGUI()
+    {
+        GUI.Window(0, windowRect, WindowFunction, "Login");
+    }
+
+    void WindowFunction (int windowID)
+    {
+        // User input 
+        usernameString = GUI.TextField(new Rect(Screen.width/3, 2*Screen.height/5, Screen.width / 3, Screen.height / 10), usernameString, 10);
+        passwordString = GUI.PasswordField(new Rect(Screen.width/3, 2*Screen.height/3, Screen.width / 3, Screen.height / 10), passwordString, "*"[0] , 10);
+
+        if(GUI.Button(new Rect(Screen.width/2, 4*Screen.height/5, Screen.width/8, Screen.height/10), "Log-in"))
+        {
+            if (usernameString == u && passwordString == p)
+            {
+                Debug.Log("Welcome to the AR Collaboration App!");
+                GUI.Label(new Rect(100, 100, 400, 30), "Welcome to the AR Collaboration App!");
+            }
+            else { 
+                GUI.Label(new Rect(Screen.width/3, 45*Screen.height/100, Screen.width/6, Screen.height/6), "Wrong username or password!");
+                Debug.Log("Wrong username or password!");
+
+            }
+        }
+
+        GUI.Label(new Rect(Screen.width / 3, 35 * Screen.height / 100, Screen.width / 8, Screen.height / 8), "Username");
+        GUI.Label(new Rect(Screen.width / 3, 62 * Screen.height / 100, Screen.width / 8, Screen.height / 8), "Password");
+
+
+    }
+
+
+}
---
+++ AR Collaboration/Assets/Register.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+using System.Text.RegularExpressions;
+
+public class Register : MonoBehaviour {
+	public GameObject username;
+	//public GameObject email;
+	public GameObject password;
+	public GameObject confPassword;
+	private string Username;
+	//private string Email;
+	private string Password;
+	private string ConfPassword;
+	private string form;
+	//private bool EmailValid = false;
+	private string[] Characters = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+								   "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+								   "1","2","3","4","5","6","7","8","9","0","_","-"};
+	
+	public void RegisterButton(){
+		bool UN = false;
+		//bool EM = false;
+		bool PW = false;
+		bool CPW = false;
+
+		if (Username != ""){
+			if (!System.IO.File.Exists(@"E:/UnityTestFolder/"+Username+".txt")){
+				UN = true;
+			} else {
+				Debug.LogWarning("Username Taken");
+			}
+		} else {
+			Debug.LogWarning("Username field Empty");
+		}
+
+
+		/*if (Email != ""){
+			EmailValidation();
+			if (EmailValid){
+				if(Email.Contains("@")){
+					if(Email.Contains(".")){
+						EM = true;
+					} else {
+						Debug.LogWarning("Email is Incorrect");
+					}
+				} else {
+					Debug.LogWarning("Email is Incorrect");
+				}
+			} else {
+				Debug.LogWarning("Email is Incorrect");
+			}
+		} else {
+			Debug.LogWarning("Email Field Empty");
+		}*/
+
+
+		if (Password != ""){
+			if(Password.Length > 5){
+				PW = true;
+			} else {
+				Debug.LogWarning("Password Must Be atleast 6 Characters long");
+			}
+		} else {
+			Debug.LogWarning("Password Field Empty");
+		}
+
+
+		if (ConfPassword != ""){
+			if (ConfPassword == Password){
+				CPW = true;
+			} else {
+				Debug.LogWarning("Passwords Don't Match");
+			}
+		} else {
+			Debug.LogWarning("Confirm Password Field Empty");
+		}
+
+        //if (UN == true&&EM == true&&PW == true&&CPW == true){
+		if (UN == true&&PW == true&&CPW == true){
+			bool Clear = true;
+			int i = 1;
+			foreach(char c in Password){
+				if (Clear){
+					Password = "";
+					Clear = false;
+				}
+				i++;
+				char Encrypted = (char)(c * i);
+				Password += Encrypted.ToString();
+			}
+			form = (Username+Environment.NewLine+Password);
+            //form = (Username + Environment.NewLine + Email + Environment.NewLine + Password);
+            System.IO.File.WriteAllText(@"E:/UnityTestFolder/"+Username+".txt", form);
+			username.GetComponent<InputField>().text = "";
+			//email.GetComponent<InputField>().text = "";
+			password.GetComponent<InputField>().text = "";
+			confPassword.GetComponent<InputField>().text = "";
+			print ("Registration Complete");
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Tab)){
+			if (username.GetComponent<InputField>().isFocused){
+				//email.GetComponent<InputField>().Select();
+                password.GetComponent<InputField>().Select();
+            }
+			/*if (email.GetComponent<InputField>().isFocused){
+				password.GetComponent<InputField>().Select();
+			}*/
+			if (password.GetComponent<InputField>().isFocused){
+				confPassword.GetComponent<InputField>().Select();
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.Return)){
+            //if (Password != ""&&Email != ""&&Password != ""&&ConfPassword != ""){
+			if (Password != ""&&Password != ""&&ConfPassword != ""){
+				RegisterButton();
+			}
+		}
+		Username = username.GetComponent<InputField>().text;
+		//Email = email.GetComponent<InputField>().text;
+		Password = password.GetComponent<InputField>().text;
+		ConfPassword = confPassword.GetComponent<InputField>().text;
+	}
+
+/*	void EmailValidation(){
+		bool SW = false;
+		bool EW = false;
+		for(int i = 0;i<Characters.Length;i++){
+			if (Email.StartsWith(Characters[i])){
+				SW = true;
+			}
+		}
+		for(int i = 0;i<Characters.Length;i++){
+			if (Email.EndsWith(Characters[i])){
+				EW = true;
+			}
+		}
+		if(SW == true&&EW == true){
+			EmailValid = true;
+		} else {
+			EmailValid = false;
+		}
+
+	}*/
+}
