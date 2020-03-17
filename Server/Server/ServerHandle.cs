57a44845600806b1c58ccae62978a3065e8bf7dd
unknown <miurary@oregonstate.edu>
2020-03-16 21:40:01 -0700

    Adding server code

---
+++ Server/Server/ServerHandle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace Server
+{
+    class ServerHandle
+    {
+        public static void WelcomeReceived(int _fromClient, Packet _packet)
+        {
+            int _clientIdCheck = _packet.ReadInt();
+            string _username = _packet.ReadString();
+
+            Console.WriteLine($"{Server.clients[_fromClient].tcp.socket.Client.RemoteEndPoint} connected successfully and is now Player {_fromClient}: {_username}.");
+            if (_fromClient != _clientIdCheck)
+            {
+                Console.WriteLine($"Player \"{_username}\" (ID: {_fromClient}) has assumed the wrong client ID ({_clientIdCheck})!");
+            }
+            Server.clients[_fromClient].SendIntoGame(_username);
+        }
+
+        public static void PlayerMovement(int _fromClient, Packet _packet)
+        {
+            bool[] _inputs = new bool[_packet.ReadInt()];
+            for (int i = 0; i < _inputs.Length; i++)
+            {
+                _inputs[i] = _packet.ReadBool();
+            }
+
+            Quaternion _rotation = _packet.ReadQuaternion();
+
+            Server.clients[_fromClient].player.SetInput(_inputs, _rotation);
+        }
+
+        public static void PlayerAudio(int _fromClient, Packet _packet)
+        {
+            Console.WriteLine($"Receiving audio at {DateTime.Now}");
+            float[] _samples = new float[_packet.ReadInt()];
+            for (int i = 0; i < _samples.Length; i++)
+            {
+                _samples[i] = _packet.ReadFloat();
+            }
+
+            Server.clients[_fromClient].player.SetAudio(_samples);
+        }
+
+        /*public static void UDPTestReceived(int _fromClient, Packet _packet)
+        {
+            string _msg = _packet.ReadString();
+
+            Console.WriteLine($"Received packet via UDP. Contains message: {_msg}");
+        }*/
+    }
+}
---
+++ Server/Server/TCP.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class TCP
+    {
+        public static int dataBufferSize = 4096;
+
+        public TcpClient socket;
+
+        private readonly int id;
+        private NetworkStream stream;
+        private byte[] receiveBuffer;
+        private Packet receivedData;
+
+        public TCP(int _id)
+        {
+            id = _id;
+        }
+
+        public void Connect(TcpClient _socket)
+        {
+            socket = _socket;
+            socket.ReceiveBufferSize = dataBufferSize;
+            socket.SendBufferSize = dataBufferSize;
+
+            stream = socket.GetStream();
+
+            receivedData = new Packet();
+            receiveBuffer = new byte[dataBufferSize];
+
+            stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+
+            ServerSend.Welcome(id, "Hello is this thing on?");
+        }
+
+        public void SendData(Packet _packet)
+        {
+            try
+            {
+                if (socket != null)
+                {
+                    stream.BeginWrite(_packet.ToArray(), 0, _packet.Length(), null, null);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error sending data to player {id} via TCP: {e}");
+            }
+        }
+
+        private void ReceiveCallback(IAsyncResult _result)
+        {
+            try
+            {
+                int _byteLength = stream.EndRead(_result);
+                if (_byteLength <= 0)
+                {
+                    return;
+                }
+
+                byte[] _data = new byte[_byteLength];
+                Array.Copy(receiveBuffer, _data, _byteLength);
+
+                receivedData.Reset(HandleData(_data));
+                stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error receiving TCP data: {e}");
+            }
+        }
+
+        private bool HandleData(byte[] _data)
+        {
+            int _packetLength = 0;
+
+            receivedData.SetBytes(_data);
+
+            if (receivedData.UnreadLength() >= 4)
+            {
+                _packetLength = receivedData.ReadInt();
+                if (_packetLength <= 0)
+                {
+                    return true;
+                }
+            }
+
+            while (_packetLength > 0 && _packetLength <= receivedData.UnreadLength())
+            {
+                byte[] _packetBytes = receivedData.ReadBytes(_packetLength);
+                ThreadManager.ExecuteOnMainThread(() =>
+                {
+                    using (Packet _packet = new Packet(_packetBytes))
+                    {
+                        int _packetId = _packet.ReadInt();
+                        Server.packetHandlers[_packetId](id, _packet);
+                    }
+                });
+
+                _packetLength = 0;
+                if (receivedData.UnreadLength() >= 4)
+                {
+                    _packetLength = receivedData.ReadInt();
+                    if (_packetLength <= 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            if (_packetLength <= 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
