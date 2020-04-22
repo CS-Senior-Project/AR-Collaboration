a5950ca8941e62491465f5f6f2dedab9f8e3d2be
BarryLe <35274721+BarryLe@users.noreply.github.com>
2020-04-22 16:10:49 -0500

    cross laser with BCP obj
    use collider to detect the position

---
+++ AR Collaboration/Assets/Scripts/collidercheck.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class collidercheck : MonoBehaviour
+{
+    public GameObject player;
+
+    private void Start()
+    {
+        Physics.IgnoreCollision(GetComponent<Collider>(),player.GetComponent<Collider>());
+    }
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("!!!laser destroy"+collision.collider.name);
+    }
+}
---
+++ AR Collaboration/Assets/Scripts/mycollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class mycollision : MonoBehaviour
+{
+    Vector3 scale=new Vector3(0.2f, 0.2f, 0.2f);
+    void OnTriggerEnter(Collider collision)
+    {
+
+            Debug.Log(collision.name + collision.transform.position);
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        cube.transform.localScale = scale;
+        cube.transform.position = collision.transform.position;
+        cube.GetComponent<Collider>().enabled=false;
+        cube.transform.SetParent(transform);
+        
+    }
+
+    private void Update()
+    {
+      
+    }
+}
