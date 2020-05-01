5a927d583ca78b5380987543947460ee51bfaf4d
BarryLe <35274721+BarryLe@users.noreply.github.com>
2020-04-30 20:59:14 -0500

    new commments

--- AR Collaboration/Assets/Scripts/raycasting.cs
+++ AR Collaboration/Assets/Scripts/raycasting.cs
@@ -6,17 +6,24 @@ using Valve.VR;
 public class raycasting : MonoBehaviour
 {
     Vector3 scale = new Vector3(0.05f, 0.05f, 0.05f);
+    //reference  to the action 
     public SteamVR_Action_Boolean Trigger;
+    //reference of the input type
     public SteamVR_Input_Sources handType;
 
     // Update is called once per frame
     void Update()
     {
+        // the hit object which store the information of hitten object
         RaycastHit hit;
+
+        //The Raycast will return true with start position vector and diretion vector. iff the collide object is "Cube"
         if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity) && hit.collider.gameObject.name == "Cube")
         {
+        //handle the handler trigger input 
             if (GetTrigger()) { 
             Debug.Log("hit object:" + hit.collider.gameObject.name + "xyz:" + hit.point);
+         //create a sphere object as the child object of object been hitten("Cube")
             GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             sphere.transform.localScale = scale;
             sphere.transform.position = hit.point;
@@ -25,6 +32,7 @@ public class raycasting : MonoBehaviour
             }
         }
     }
+
 
     public bool GetTrigger() 
     {
