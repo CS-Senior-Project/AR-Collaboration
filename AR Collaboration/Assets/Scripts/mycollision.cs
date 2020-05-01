c2a96f1b80ca90ac274be5ed77ee4c5c3f0c5884
BarryLe <35274721+BarryLe@users.noreply.github.com>
2020-04-30 20:20:00 -0500

    raycasting script in scene Barry-cross1

--- AR Collaboration/Assets/Scripts/mycollision.cs
+++ AR Collaboration/Assets/Scripts/mycollision.cs
@@ -3,21 +3,22 @@ using UnityEngine;
 
 public class mycollision : MonoBehaviour
 {
+    //hardcode scale value
     Vector3 scale=new Vector3(0.2f, 0.2f, 0.2f);
+
+    //build in call back function when an isTrigger object get hitted
     void OnTriggerEnter(Collider collision)
     {
-
-            Debug.Log(collision.name + collision.transform.position);
+        Debug.Log(collision.name + collision.transform.position);
+        //create a sphere object
         GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         cube.transform.localScale = scale;
+
+        //pull the collider position
         cube.transform.position = collision.transform.position;
         cube.GetComponent<Collider>().enabled=false;
         cube.transform.SetParent(transform);
-        
     }
 
-    private void Update()
-    {
-      
-    }
+
 }
---
+++ AR Collaboration/Assets/Scripts/raycasting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycasting : MonoBehaviour
+{
+    Vector3 scale = new Vector3(0.05f, 0.05f, 0.05f);
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity) && hit.collider.gameObject.name == "Cube")
+        {
+                Debug.Log("hit object:" + hit.collider.gameObject.name + "xyz:" + hit.point);
+                GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                sphere.transform.localScale = scale;
+                sphere.transform.position = hit.point;
+                sphere.GetComponent<Collider>().enabled = false;
+                sphere.transform.SetParent(hit.collider.gameObject.transform); 
+        }
+    }
+}
